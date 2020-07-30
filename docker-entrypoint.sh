c1af1df1345f71d315071d5fd93c4d67e650333b
Lei Zhang <zhanglei@apache.org>
2020-07-30 13:08:11 +0800

    init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,40 +3,48 @@ echo "============================="
 set -eo pipefail
 shopt -s nullglob
 
+init_config_path() {
+  if [ ! -d "/etc/ansible" ]; then
+    mkdir -p /etc/ansible
+  fi
+}
+
 init_ansible_cfg() {
-  mkdir -p /etc/ansible
-  echo '[defaults]' > /etc/ansible/ansible.cfg
-  echo 'host_key_checking=false' >> /etc/ansible/ansible.cfg
-  echo 'command_warnings=false' >> /etc/ansible/ansible.cfg
+  if [ ! -d "/etc/ansible/ansible.cfg" ]; then
+    echo '[defaults]' > /etc/ansible/ansible.cfg
+    echo 'host_key_checking=false' >> /etc/ansible/ansible.cfg
+    echo 'command_warnings=false' >> /etc/ansible/ansible.cfg
+  fi
 }
 
 init_ansible_hosts() {
-  if [ "$ANSIBLE_SSH_HOSTS" != "" ]; then
+  if [ ! -d "/etc/ansible/hosts" ]; then
+    if [ "$ANSIBLE_SSH_HOSTS" != "" ]; then
+      echo '[default]' > /etc/ansible/hosts
 
-    mkdir -p /etc/ansible
-    echo '[default]' > /etc/ansible/hosts
+      IFS=', ' read -r -a ANSIBLE_SSH_HOSTS_ARRAY <<< $ANSIBLE_SSH_HOSTS
+      IFS=', ' read -r -a ANSIBLE_SSH_PORTS_ARRAY <<< $ANSIBLE_SSH_PORTS
+      IFS=', ' read -r -a ANSIBLE_SSH_USER_ARRAY <<< $ANSIBLE_SSH_USER
+      IFS=', ' read -r -a ANSIBLE_SSH_PASS_ARRAY <<< $ANSIBLE_SSH_PASS
+      IFS=', ' read -r -a ANSIBLE_SU_PASS_ARRAY <<< $ANSIBLE_SU_PASS
 
-    IFS=', ' read -r -a ANSIBLE_SSH_HOSTS_ARRAY <<< $ANSIBLE_SSH_HOSTS
-    IFS=', ' read -r -a ANSIBLE_SSH_PORTS_ARRAY <<< $ANSIBLE_SSH_PORTS
-    IFS=', ' read -r -a ANSIBLE_SSH_USER_ARRAY <<< $ANSIBLE_SSH_USER
-    IFS=', ' read -r -a ANSIBLE_SSH_PASS_ARRAY <<< $ANSIBLE_SSH_PASS
-    IFS=', ' read -r -a ANSIBLE_SU_PASS_ARRAY <<< $ANSIBLE_SU_PASS
+      for index in "${!ANSIBLE_SSH_HOSTS_ARRAY[@]}"
+      do
+        NAME = ${ANSIBLE_SSH_HOSTS_ARRAY[index]}
+        HOST = ${ANSIBLE_SSH_HOSTS_ARRAY[index]}
+        PORT = ${ANSIBLE_SSH_PORTS_ARRAY[index]}
+        USER = ${ANSIBLE_SSH_USER_ARRAY[index]}
+        PASS = ${ANSIBLE_SSH_PASS_ARRAY[index]}
+        SU_PASS = ${ANSIBLE_SU_PASS_ARRAY[index]}
 
-    for index in "${!ANSIBLE_SSH_HOSTS_ARRAY[@]}"
-    do
-      NAME = ${ANSIBLE_SSH_HOSTS_ARRAY[index]}
-      HOST = ${ANSIBLE_SSH_HOSTS_ARRAY[index]}
-      PORT = ${ANSIBLE_SSH_PORTS_ARRAY[index]}
-      USER = ${ANSIBLE_SSH_USER_ARRAY[index]}
-      PASS = ${ANSIBLE_SSH_PASS_ARRAY[index]}
-      SU_PASS = ${ANSIBLE_SU_PASS_ARRAY[index]}
-
-      echo '${NAME} ansible_ssh_host=${HOST} ansible_ssh_port=${PORT} ansible_ssh_user=${USER} ansible_ssh_pass=${PASS} ansible_become_pass=${SU_PASS}' >> /etc/ansible/hosts
-    done
+        echo '${NAME} ansible_ssh_host=${HOST} ansible_ssh_port=${PORT} ansible_ssh_user=${USER} ansible_ssh_pass=${PASS} ansible_become_pass=${SU_PASS}' >> /etc/ansible/hosts
+      done
+    fi    
   fi
 }
 
 _main() {
+  init_config_path
   init_ansible_cfg
   init_ansible_hosts
   exec $@
