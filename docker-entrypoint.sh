17f62cc2bc5459ca7467c9d4a1c2944c455caea5
Lei Zhang <zhanglei@apache.org>
2020-07-30 15:16:40 +0800

    init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,21 +12,21 @@ init_config_path() {
 
 init_ansible_cfg() {
   if [ ! -d "/etc/ansible/ansible.cfg" ]; then
-    echo 'Create /etc/ansible/ansible.cfg'
-    echo '[defaults]' > /etc/ansible/ansible.cfg
-    echo 'host_key_checking=false' >> /etc/ansible/ansible.cfg
-    echo 'command_warnings=false' >> /etc/ansible/ansible.cfg
+    echo "Create /etc/ansible/ansible.cfg"
+    echo "[defaults]" > /etc/ansible/ansible.cfg
+    echo "host_key_checking=false" >> /etc/ansible/ansible.cfg
+    echo "command_warnings=false" >> /etc/ansible/ansible.cfg
   else
-    echo 'Load /etc/ansible/ansible.cfg'
+    echo "Load /etc/ansible/ansible.cfg"
   fi
 }
 
 init_ansible_hosts() {
   if [ ! -d "/etc/ansible/hosts" ]; then
     if [ "$ANSIBLE_SSH_HOSTS" != "" ]; then
-      echo 'Create /etc/ansible/hosts'
+      echo "Create /etc/ansible/hosts"
 
-      echo '[default]' > /etc/ansible/hosts
+      echo "[default]" > /etc/ansible/hosts
 
       IFS=', ' read -r -a ANSIBLE_SSH_HOSTS_ARRAY <<< $ANSIBLE_SSH_HOSTS
       IFS=', ' read -r -a ANSIBLE_SSH_PORTS_ARRAY <<< $ANSIBLE_SSH_PORTS
@@ -42,12 +42,12 @@ init_ansible_hosts() {
         USER=${ANSIBLE_SSH_USER_ARRAY[index]}
         PASS=${ANSIBLE_SSH_PASS_ARRAY[index]}
         SU_PASS=${ANSIBLE_SU_PASS_ARRAY[index]}
-        echo 'add HOST $NAME'
-        echo '$NAME ansible_ssh_host=$HOST ansible_ssh_port=$PORT ansible_ssh_user=$USER ansible_ssh_pass=$PASS ansible_become_pass=$SU_PASS' >> /etc/ansible/hosts
+        echo "add HOST $NAME"
+        echo "$NAME ansible_ssh_host=$HOST ansible_ssh_port=$PORT ansible_ssh_user=$USER ansible_ssh_pass=$PASS ansible_become_pass=$SU_PASS" >> /etc/ansible/hosts
       done
     fi
   else
-    echo 'Load /etc/ansible/hosts'
+    echo "Load /etc/ansible/hosts"
   fi
 }
 
